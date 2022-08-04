ab75450f80b551d8da7d9d56653f973ce6a8a16f
fanxifen <fanxifen@outlook.com>
2022-08-05 01:17:33 +0800

    edit ui

--- packages/next-app/pages/_app.js
+++ packages/next-app/pages/_app.js
@@ -7,7 +7,7 @@ import { wagmiClient, chains } from "../helpers/rainbowSetup";
 
 function MyApp({ Component, pageProps }) {
   const appInfo = {
-    appName: "🦄Web3 Starter Kit",
+    appName: "🦄Sign On web3",
   };
 
   return (
